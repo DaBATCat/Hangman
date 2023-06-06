6a07601f8fd6b79a46df92af2f4b0fdb4d9d4255
Dama1206 <93281223+Dama1206@users.noreply.github.com>
2023-06-06 15:39:28 +0200

    Update SQLiteConnection.java
    not complete

--- Hangman/src/SQLiteConnection.java
+++ Hangman/src/SQLiteConnection.java
@@ -5,27 +5,47 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class SQLiteConnection {
-  public static void main(String[] args) throws SQLException {
-    Connection connection = null;
-    String url = "jdbc:sqlite:hangman.db";
-    try {
+    String query = "select word from words";
+    static Connection connection = null;
+    static String url = "jdbc:sqlite:hangman.db";
+    public static void main(String[] args) throws SQLException {
+        // String[] a = getWordsFromTable();
+    /*
+        try {
       connection = DriverManager.getConnection(url);
       printtln("Connection successfull!");
       // executions here
-      String query = "Select * from words";
-      new SQLiteConnection().executeStatements(connection, query, true) ;
+
+      // new SQLiteConnection().executeStatements(connection, query, true) ;
+        new SQLiteConnection().executeStatements(connection, new SQLiteConnection().query, false);
     }
     catch (SQLException e){
       e.printStackTrace();
     }
+    */
+        connection = DriverManager.getConnection(url);
+        new SQLiteConnection().executeStatements(connection, new SQLiteConnection().query, false);
     assert connection != null;
     connection.close();
     printtln("Closed connection");
-
-
-
-
-  }
+ }
+ public static String[] getWordsFromTable(){
+    try {
+        connect(url);
+        Statement statement = connection.createStatement();
+        statement.executeUpdate("Select word from words");
+        ResultSet resultSet = statement.executeQuery("Select word from words");
+        String[] result = new String[5];
+        for(int i = 1; i < 6; i++){
+            // result[i] = resultSet.getString(1, i);
+        }
+        return result;
+    }
+    catch (SQLException e) {
+        e.printStackTrace();
+    }
+    throw new IllegalArgumentException();
+ }
 
   // for default executions
   public void executeStatements(Connection connection, String query, boolean printTable) throws SQLException {
@@ -33,6 +53,10 @@ public class SQLiteConnection {
     statement.executeUpdate(query);
     printtln("Query executed! (" + query + ")");
     if(printTable)printTable(statement);
+    else{
+        ResultSet resultSet = statement.executeQuery(new SQLiteConnection().query);
+        System.out.println(resultSet.getString(1));
+    }
   }
 
   // called executions here
@@ -45,7 +69,7 @@ public class SQLiteConnection {
   //}
 
   private void printTable(Statement statement) throws SQLException {
-    ResultSet resultSet = statement.executeQuery("select * from words;");
+    ResultSet resultSet = statement.executeQuery(new SQLiteConnection().query);
     printtln("Printing Table:");
     while(resultSet.next()){
       System.out.println("ID\tword");
@@ -64,5 +88,10 @@ public class SQLiteConnection {
   /* Table name: words
    * attributes: (id#, word)
    */
+
+    private static void connect(String url) throws SQLException {
+        connection = DriverManager.getConnection(url);
+        printtln("Connection successfull!");
+    }
 
 }
