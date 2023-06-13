5c9575c7e6342b7ba78f36e76d76297a71ddb146
Xrasure <60069045+Xrasure@users.noreply.github.com>
2023-06-13 15:43:38 +0200

    Spieler zum Spiel hinzugefügt

--- Hangman/src/Main.java
+++ Hangman/src/Main.java
@@ -5,7 +5,9 @@ import java.sql.SQLException;
 public class Main {
   public static void main(String[] args) throws SQLException {
     Model model = new Model();
-    model.firstEzGame();
+    Spieler spieler1 = new Spieler("ja","nein");
+    model.firstEzGame(spieler1);
+    System.out.println("siegesquote:"+spieler1.getSiegesquote()+"%");
     SQLiteConnection sqLiteConnection = new SQLiteConnection();
     for(int i = 0; i < sqLiteConnection.getWordsFromTable().size(); i++){
       System.out.println(sqLiteConnection.getWordsFromTable().get(i));
