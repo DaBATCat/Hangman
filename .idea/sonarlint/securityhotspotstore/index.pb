de22d8db415aa1d8cacb6c27a6327ba16335edf2
Dama1206 <93281223+Dama1206@users.noreply.github.com>
2023-06-17 23:26:51 +0200

    recreated hangman.db and added functions to new table

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,22 @@
+
+K
+Hangman/src/LoggerTest.java,1\9\19bd4d57bf2b01cdd1f057d181b8b9ed179fb9ff
+T
+$Hangman/src/CallbackPrintStream.java,6\b\6bb10ba7b99c88e39f297344237eddd6478264fe
+G
+Hangman/src/Logger.java,a\0\a04e2ede8378236aeaa91c00f263a72001edcd7b
+R
+"Hangman/src/CustomPrintStream.java,7\2\7200a57654f0bc1b357eee114e7e482a49a29356
+P
+ Hangman/src/MySQLConnection.java,9\3\939ee5800f42806d7981867984181fe8586e1764
+Q
+!Hangman/src/SQLiteConnection.java,1\e\1e66f0db4ad549cfdb21734245ba1b533ec61df3
+E
+Hangman/src/Main.java,5\0\50d253e37e068f93a72015a456a6d94d232d39ed
+F
+Hangman/src/Model.java,8\4\84cf2a95c6f89f609e0702650be023aa010a6e06
+C
+Hangman/Hangman.iml,2\e\2e80ac82f137bf4c38810841139e64d73e8b83a0
+:
+
+hangman.db,f\f\ffe3a0c1c88847cf75721e2cd24b7771f3753cd1
--- Hangman/src/SQLiteConnection.java
+++ Hangman/src/SQLiteConnection.java
@@ -6,24 +6,63 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class SQLiteConnection {
-    static String query = "select * from words";
+    // hangman.db:
+
+    // Name der Tabelle: words
+    // Spalten der Tabelle: id (PK) | word
+    // -----------------------------------------------------------
+    // management:
+    // Name der Tabelle: management
+    // Spalten der Tabelle: username varchar(32) (PK) | passwort varchar(64) | spiele int | wins int | losses int
+    static enum Table {
+      WORDS,
+      MANAGEMENT
+    }
+    static String query = "select * from management";
     static Connection connection = null;
-    static String url = "jdbc:sqlite:hangman.db";
+    static String url ;
     public static void main(String[] args) throws SQLException {
+      // Welche Datenbank wird angesteuert?
+      url = "jdbc:sqlite:hangman.db";
+
         try {
-      connection = DriverManager.getConnection(url);
-      printtln("Connection successfull!");
+      connect(url);
+
       // executions here
-          new SQLiteConnection().printWords(connection);
+          // new SQLiteConnection().createUser("Daniel", "penis1");
+          new SQLiteConnection().printTable(Table.MANAGEMENT);
     }
     catch (SQLException e){
+          printtln("Error!");
       e.printStackTrace();
     }
-      System.out.println(new SQLiteConnection().getWords(connection)[1]);
 
     assert connection != null;
     connection.close();
     printtln("Closed connection");
+ }
+
+ public void addWord(String word) throws SQLException {
+   Statement statement = connection.createStatement();
+   String tableQuery;
+   tableQuery = String.format("insert into words (word) values ('%s')", word);
+   statement.executeUpdate(tableQuery);
+   printtln("Query executed! (" + tableQuery + ")");
+ }
+
+ public void addUser(String username, String password) throws SQLException {
+   Statement statement = connection.createStatement();
+   String tableQuery;
+   tableQuery = String.format("insert into management values(" +
+           "'%s', '%s', 0, 0, 0);", username, password);
+   statement.executeUpdate(tableQuery);
+   printtln("Query executed! (" + tableQuery + ")");
+
+ }
+
+ public void createUser(String username, String password) throws SQLException {
+      String query = String.format("insert into management values ('%s', '%s', 0, 0, 0);", username, password);
+    new SQLiteConnection().executeSimpleQuery(query);
  }
 
  // Get all Words here
@@ -39,7 +78,7 @@ public class SQLiteConnection {
     return words;
   }
 
- public static String[] getWordsFromTable(Connection connection) throws SQLException{
+ public static String[] getWordsFromWordsTable(Connection connection) throws SQLException{
         connect(url);
         Statement statement = connection.createStatement();
         statement.executeUpdate("Select word from words");
@@ -52,11 +91,12 @@ public class SQLiteConnection {
  }
 
   // for default executions
+  // veraltet, nur für words
   public void executeStatements(Connection connection, String query, boolean printTable) throws SQLException {
     Statement statement = connection.createStatement();
     statement.executeUpdate(query);
     printtln("Query executed! (" + query + ")");
-    if(printTable)printTable(statement);
+    if(printTable)printTable(Table.WORDS);
     else{
         ResultSet resultSet = statement.executeQuery(new SQLiteConnection().query);
         System.out.println(resultSet.getString(1));
@@ -72,13 +112,34 @@ public class SQLiteConnection {
   //  executeStatements(connection, "insert into ", true);
   //}
 
-  private void printTable(Statement statement) throws SQLException {
-    ResultSet resultSet = statement.executeQuery(query);
-    printtln("Printing Table:");
-    System.out.println("ID\tword");
-    while(resultSet.next()){
-      System.out.println(resultSet.getInt(1) + "\t" + resultSet.getString(2));
+  //TODO: Dingsbums management abfrage
+  private void printTable(Table table) throws SQLException {
+    Statement statement = connection.createStatement();
+    String tableQuery;
+    if(table == Table.WORDS) {
+      tableQuery = "select * from words";
+      statement.executeUpdate(tableQuery);
+      printtln("Query executed! (" + tableQuery + ")");
+      ResultSet resultSet = statement.executeQuery(tableQuery);
+
+      printtln("Printing Table: " + table.toString().toLowerCase());
+      System.out.println("ID\tword");
+      while (resultSet.next()) {
+        System.out.println(resultSet.getInt(1) + "\t" + resultSet.getString(2));
+      }
     }
+    else{
+      tableQuery = "select * from management";
+      statement.executeUpdate(tableQuery);
+      printtln("Query executed! (" + tableQuery + ")");
+      ResultSet resultSet = statement.executeQuery(tableQuery);
+
+      System.out.println("username\tpassword\tspiele\twins\tlosses");
+      System.out.println(resultSet.getString(1) + "\t" + resultSet.getString(2) + "\t"+
+              resultSet.getInt(3) + "\t" + resultSet.getInt(4) + "\t" + resultSet.getInt(5));
+
+    }
+
 
   }
   private void printWords(Connection connection) throws SQLException{
@@ -120,6 +181,13 @@ public class SQLiteConnection {
     private static void connect(String url) throws SQLException {
         connection = DriverManager.getConnection(url);
         printtln("Connection successfull!");
+    }
+
+    public void executeSimpleQuery(String query) throws SQLException {
+      Statement statement = connection.createStatement();
+      statement.executeUpdate(query);
+      printtln("Query executed! (" + query + ")");
+      //ResultSet resultSet = statement.executeQuery(query);
     }
 
 }
